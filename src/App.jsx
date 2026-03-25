0f770aa0758415d910a566b43622c2373530a176
RushiGujarathi <rushigujarathi1717@gmail.com>
2026-03-25 12:31:28 +0530

    fix vercel error

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ export default function App() {
         {/* Floating overlays */}
         <AchievementSystem />
         <MarsAIGuide />
-        <EasterEggs />
+      
 
         <main>
           <Hero missionPath={missionPath} />
